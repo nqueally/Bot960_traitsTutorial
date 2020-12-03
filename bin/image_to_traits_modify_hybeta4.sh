0769373402ce7ca1b3cf7fbe1147ed2e0aa75989
nqueally <40647013+nqueally@users.noreply.github.com>
2020-12-02 18:05:15 -0800

    Update image_to_traits_modify_hybeta4.sh

--- bin/image_to_traits_modify_hybeta4.sh
+++ bin/image_to_traits_modify_hybeta4.sh
@@ -12,7 +12,17 @@ mkdir $ENVDIR
 tar -xzf $ENVNAME.tar.gz -C $ENVDIR
 . $ENVDIR/bin/activate
 
+##  get command line params
+imgdir=$1
+obsdir=$2
+outputdir=$3
+topo_coeff=$4
+brdf_coeff=$5
+mask_t=$6
+outname=$7
+trait_coeff=$8
+
 # modify this line to run your desired Python script and any other work you need to do
 # import gdal as a check on the python environment;
-python -c 'from osgeo import gdal; print("Hello, World"); print("faking out image_to_traits_modify_hybeta4")'
+python3 image_to_traits_modify_hybeta4.py  --img $imgdir --obs $obsdir --od $outputdir --topo $topo_coeff --brdf $brdf_coeff --mask --mask_threshold $mask_t --out $outname --rgbim --coeffs $trait_coeff
 
