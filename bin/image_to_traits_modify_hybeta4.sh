91edc7cde59004603fabf3de65e7e81c21f2ccc4
nqueally <40647013+nqueally@users.noreply.github.com>
2020-12-04 17:58:39 -0600

    Update image_to_traits_modify_hybeta4.sh

--- bin/image_to_traits_modify_hybeta4.sh
+++ bin/image_to_traits_modify_hybeta4.sh
@@ -13,8 +13,8 @@ tar -xzf $ENVNAME.tar.gz -C $ENVDIR
 . $ENVDIR/bin/activate
 
 ##  get command line params
-imgdir=$1
-obsdir=$2
+img=$1
+obs=$2
 outputdir=$3
 topo_coeff=$4
 brdf_coeff=$5
@@ -22,7 +22,10 @@ mask_t=$6
 outname=$7
 trait_coeff=$8
 
+cp $basedir/$img* .
+cp $basedir/$obs* . 
+
 # modify this line to run your desired Python script and any other work you need to do
 # import gdal as a check on the python environment;
-python3 image_to_traits.py  --img $imgdir --obs $obsdir --od $outputdir --topo $topo_coeff --brdf $brdf_coeff --mask --mask_threshold $mask_t --out $outname --rgbim --coeffs $trait_coeff
+python3 image_to_traits.py  --img $img --obs $obs --od $outputdir --topo $topo_coeff --brdf $brdf_coeff --mask --mask_threshold $mask_t --out $outname --rgbim --coeffs $trait_coeff
 
