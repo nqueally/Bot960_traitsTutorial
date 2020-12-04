58c94277116c83599af0ddb4e6a607e661a9b90b
nqueally <40647013+nqueally@users.noreply.github.com>
2020-12-04 14:21:31 -0600

    Update topo_brdf_coeffs_modify_hybeta4.sh

--- bin/topo_brdf_coeffs_modify_hybeta4.sh
+++ bin/topo_brdf_coeffs_modify_hybeta4.sh
@@ -30,5 +30,5 @@ cp $basedir/$obs* .
 # modify this line to run your desired Python script and any other work you need to do
 # import gdal as a check on the python environment;
 #python -c 'from osgeo import gdal; print("Hello, World!"); print("faking topo_brdf")'
-python3 topo_brdf_coeffs_modify_hybeta4.py  --img $img --obs $obs --od $outputdir --pref $pref --kernels $kernel --mask --mask_threshold $mask_t --topo --brdf 
+python3 topo_brdf_coeffs.py  --img $img --obs $obs --od $outputdir --pref $pref --kernels $kernel --mask --mask_threshold $mask_t --topo --brdf 
 
